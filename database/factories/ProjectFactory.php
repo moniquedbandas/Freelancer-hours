e262d3c280fc98d921d8c48a9d26acfb5b621af9
Monique Dias Bandas <moniquedbandas@gmail.com>
2024-10-14 22:36:57 -0300

    wip2

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -5,21 +5,20 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
 class ProjectFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+
     public function definition(): array
     {
         return [
             'title' => collect(fake()->words(5))->join(' '),
-            'description' => fake()->randomHtml(),
+            'description' => "oi",
             'ends_at' => fake()->dateTimeBetween('now', '+ 3 days'),
             'status' => fake()->randomElement(['open', 'closed']),
-            'tech_stack' => fake()->randomElements(['react', 'laravel', 'vue', 'tailwind', 'javascript', 'nextjs', 'python', random_int(1, 5)]),
+            'tech_stack' => fake()->randomElements(['nodejs', 'react', 'javascript', 'vite', 'nextjs'], random_int(1, 5)),
             'created_by' => User::factory(),
         ];
     }
